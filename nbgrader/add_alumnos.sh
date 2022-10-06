7eea8f075c5b510b6375bccf37dafe8670787ecf
Luis Llana <llana@sip.ucm.es>
2022-10-06 15:18:52 +0200

    hoja2

---
+++ nbgrader/add_alumnos.sh
@@ -0,0 +1,47 @@
+nbgrader db student add  --first-name "MARTA" --last-name "ALIENDE DEL PINO" --email martaali@ucm.es martaali
+nbgrader db student add  --first-name "Julio" --last-name "Amigo Carrera" --email jamigo02@ucm.es jamigo02
+nbgrader db student add  --first-name "LUCAS" --last-name "AZNAR MENÉNDEZ" --email lucaznar@ucm.es lucaznar
+nbgrader db student add  --first-name "Laura" --last-name "Bajo Fernández" --email lbajo@ucm.es lbajo
+nbgrader db student add  --first-name "Filip" --last-name "Balik" --email fbalik@ucm.es fbalik
+nbgrader db student add  --first-name "Hugo" --last-name "Ballestero Martín" --email hugoball@ucm.es hugoball
+nbgrader db student add  --first-name "MARÍA" --last-name "BÓDEMER GUERRA" --email mbodemer@ucm.es mbodemer
+nbgrader db student add  --first-name "SANDRA" --last-name "CALVO RODRIGO" --email sandcalv@ucm.es sandcalv
+nbgrader db student add  --first-name "ÁNGELA" --last-name "CRUZ GALISTEO" --email angecruz@ucm.es angecruz
+nbgrader db student add  --first-name "JUAN" --last-name "DAZA ÁGUILA" --email jdaza01@ucm.es jdaza01
+nbgrader db student add  --first-name "Sandra" --last-name "de Azaola Viota" --email sanddeaz@ucm.es sanddeaz
+nbgrader db student add  --first-name "Laura" --last-name "Díaz Cárdenas" --email ladiaz11@ucm.es ladiaz11
+nbgrader db student add  --first-name "ÁLVARO" --last-name "DÍAZ GARCÍA-CHICO" --email alvadi05@ucm.es alvadi05
+nbgrader db student add  --first-name "Sergio" --last-name "Díez Cardo" --email sediez02@ucm.es sediez02
+nbgrader db student add  --first-name "PATRICIA" --last-name "FERNÁNDEZ SANZ" --email patfer22@ucm.es patfer22
+nbgrader db student add  --first-name "Mateo" --last-name "Ferreira Vázquez" --email matferre@ucm.es matferre
+nbgrader db student add  --first-name "Paula" --last-name "Gallego López" --email pgalle07@ucm.es pgalle07
+nbgrader db student add  --first-name "Jaime" --last-name "García Méndez" --email jaimga19@ucm.es jaimga19
+nbgrader db student add  --first-name "DANIEL" --last-name "GARCÍA RODRÍGUEZ DE SEPÚLVEDA" --email daniel33@ucm.es daniel33
+nbgrader db student add  --first-name "MIRIAM" --last-name "GÓMEZ MIGUEL" --email mirgom15@ucm.es mirgom15
+nbgrader db student add  --first-name "Max" --last-name "Gonzalez Koop" --email maxgon03@ucm.es maxgon03
+nbgrader db student add  --first-name "Angélica Dilessey" --last-name "González Ramírez" --email angeligo@ucm.es angeligo
+nbgrader db student add  --first-name "Mónica" --last-name "González de Pablo" --email mogonz10@ucm.es mogonz10
+nbgrader db student add  --first-name "Pablo" --last-name "Hernández González" --email pablhe10@ucm.es pablhe10
+nbgrader db student add  --first-name "CANDELA" --last-name "HUERTA ALONSO" --email cahuerta@ucm.es cahuerta
+nbgrader db student add  --first-name "MARÍA BELÉN" --last-name "LAVANDA ROCANO" --email mlavanda@ucm.es mlavanda
+nbgrader db student add  --first-name "Amalia" --last-name "Lobo Barcenilla" --email amalobo@ucm.es amalobo
+nbgrader db student add  --first-name "Ángela" --last-name "Lomas Anglada" --email anlomas@ucm.es anlomas
+nbgrader db student add  --first-name "Eva" --last-name "López Corazón" --email evlope15@ucm.es evlope15
+nbgrader db student add  --first-name "Iván" --last-name "López Gómez" --email ivalop08@ucm.es ivalop08
+nbgrader db student add  --first-name "DAVID" --last-name "MARTÍN ARRANZ" --email davmar28@ucm.es davmar28
+nbgrader db student add  --first-name "YAGO" --last-name "MARTÍN JEREZ" --email yagomart@ucm.es yagomart
+nbgrader db student add  --first-name "FRANCISCO" --last-name "MARTÍN MANGAS" --email franma17@ucm.es franma17
+nbgrader db student add  --first-name "Alfonso" --last-name "Martín Pérez" --email alfonm09@ucm.es alfonm09
+nbgrader db student add  --first-name "Andrea Virginia" --last-name "Moncanut Laguna" --email amonca02@ucm.es amonca02
+nbgrader db student add  --first-name "Fernando" --last-name "Moreno Ruiz" --email fernmo04@ucm.es fernmo04
+nbgrader db student add  --first-name "CARLOS JOSÉ" --last-name "MURILLAS GALLEGO" --email carlmuri@ucm.es carlmuri
+nbgrader db student add  --first-name "Lucas" --last-name "Olmedo Torrado" --email lucasolm@ucm.es lucasolm
+nbgrader db student add  --first-name "RUBÉN" --last-name "ORTUÑO IBÁÑEZ" --email ruortuno@ucm.es ruortuno
+nbgrader db student add  --first-name "Anxo" --last-name "Panero Blanco" --email anxopane@ucm.es anxopane
+nbgrader db student add  --first-name "Ignacio" --last-name "Rodríguez Arias" --email ignacr09@ucm.es ignacr09
+nbgrader db student add  --first-name "Emma" --last-name "Ruiz Serrano" --email emmaru01@ucm.es emmaru01
+nbgrader db student add  --first-name "Guillermo" --last-name "Sanz Arbeloa" --email gsanz08@ucm.es gsanz08
+nbgrader db student add  --first-name "LEONARDO JAVIER" --last-name "SARZOSA GALINDO" --email lsarzosa@ucm.es lsarzosa
+nbgrader db student add  --first-name "LUIS" --last-name "TAPIA MIGUEL" --email lutapia@ucm.es lutapia
+nbgrader db student add  --first-name "ÁLVARO" --last-name "TORTAJADA FERNÁNDEZ" --email alvtorta@ucm.es alvtorta
+nbgrader db student add  --first-name "Laura" --last-name "Valls García" --email lauvalls@ucm.es lauvalls
